092be03800af6d75e90f7a019ba6c3ac982f2551
Mathew Benson <mbenson@odysseysr.com>
2015-12-15 15:39:09 -0600

    Updated CF to remove valgrind issues.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -12,6 +12,8 @@ set(CTEST_NIGHTLY_START_TIME "00:00:00 EST")
 file(TO_CMAKE_PATH "${CMAKE_SOURCE_DIR}/memcheck.supp"  MEMORYCHECK_SUPPRESSIONS_FILE)
 set(VALGRIND_COMMAND_OPTIONS "-q --tool=memcheck --leak-check=yes --workaround-gcc296-bugs=yes --num-callers=50 --trace-children=yes --leak-check=full --track-origins=yes --gen-suppressions=all")
 set(MEMORYCHECK_COMMAND_OPTIONS "-q --tool=memcheck --leak-check=yes --workaround-gcc296-bugs=yes --num-callers=50 --trace-children=yes --leak-check=full --track-origins=yes --gen-suppressions=all")
+#set(VALGRIND_COMMAND_OPTIONS "")
+#set(MEMORYCHECK_COMMAND_OPTIONS "")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "www.windhoverlabs.com")
