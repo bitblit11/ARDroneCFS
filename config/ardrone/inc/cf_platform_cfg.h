a4285e090815c6231c7bfba5014a20737a1e9cb6
Mathew Benson <mbenson@windhoverlabs.com>
2018-06-19 13:39:07 -0500

    Updated cf platform cfg to read the table from the correct place.

--- config/ardrone/inc/cf_platform_cfg.h
+++ config/ardrone/inc/cf_platform_cfg.h
@@ -185,7 +185,7 @@
 **       The length of this string, including the NULL terminator cannot exceed 
 **       the #OS_MAX_PATH_LEN value.
 */
-#define CF_CONFIG_TABLE_FILENAME            "/cf/cf_cfgtable.tbl"
+#define CF_CONFIG_TABLE_FILENAME            "/cf/apps/cf_cfgtable.tbl"
 
 
 /**
